c6cdf23adf567fb7a1239177bbf67983554d28f1
Kyoung Hoon Kim <72896316+Choroksaek@users.noreply.github.com>
2022-05-04 18:30:44 -0400

    commitment

---
+++ Project-4/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+	$('input[type=range]').on('input', function(){
+
+		let inputType = $(this).attr('id');
+		let inputValue = $(this).val();
+
+		 if (inputType == 'wght'){
+		 	$('#font').css('--wght-axis', inputValue);
+		 }
+
+	});
+});
