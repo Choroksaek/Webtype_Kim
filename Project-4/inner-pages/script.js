8aa26c05ca25cc2da89d84abb461a93f1d7862a9
Kyoung Hoon Kim <72896316+Choroksaek@users.noreply.github.com>
2022-05-11 09:54:24 -0400

    fdsaf

--- Project-4/inner-pages/script.js
+++ Project-4/inner-pages/script.js
@@ -1,3 +1,12 @@
+const iframeEle = document.getElementById('iframe');
+const loadingEle = document.getElementById('loading');
+
+iframeEle.addEventListener('load', function () {
+    loadingEle.style.display = 'none';
+
+    iframeEle.style.opacity = 1;
+});
+
 function monthDiff(dateFrom, dateTo) {
     return dateTo.getMonth() - dateFrom.getMonth() + 
       (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
